b29f74f07e52a46b9ce83cb6ce065306dd078e16
Ricky <rickdhanota@gmail.com>
2020-09-27 21:06:32 -0700

    palindrome long algo

---
+++ longest_palindrome_med.py
@@ -0,0 +1,10 @@
+# write a function that, given a string, returns its longest palindromic substring.
+# a palindrome is defined as a string that's written the same forward and backward. Note that a single-character strings are palindromes. 
+# You can assume that there will only be one longest palindromic substring
+# Sample input; string = "abaxyzzyxf"
+# sample output; "xyzzyx"
+
+def longestPalindromicSubstring(string):
+    pass
+
+print(longestPalindromicSubstring("abaxyzzyxf"))
